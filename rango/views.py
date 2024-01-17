f75db90798477951a18037e19013b78f931b2039
Varad Kulkarni <varadkulk123@gmail.com>
2024-01-17 12:53:03 +0000

    change home page view to use template

--- rango/views.py
+++ rango/views.py
@@ -8,8 +8,13 @@ from django.http import HttpResponse
     # simple object takes in string param representing content of the page
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!\n<a href='/rango/about/'>About</a>")
+    # construct dict to pass to template engine as context
+    # note the key matches template value
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
+    # return rendered response to send to client
+    # make use of shortcut function render
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page.\n<a href='/rango/'>Index</a>")
