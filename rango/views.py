7933161a24d907e4870e9cf7d34b53b0ea3789de
Varad Kulkarni <varadkulk123@gmail.com>
2024-01-23 16:51:14 +0000

    get category data from database and pass to template

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category
+
 # each view exists here as a series of individual functions
 # each view takes in at least one argument - a HttpRequest object
     # conventionally named request
@@ -8,12 +10,17 @@ from django.http import HttpResponse
     # simple object takes in string param representing content of the page
 
 def index(request):
-    # construct dict to pass to template engine as context
-    # note the key matches template value
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # query database for a list of all cateogires currently stored
+    # order categories by number of likes, descending
+    # retrieve top 5 only, or all if less than 5
+    # place list in context_dict that will be passed to template
+    category_list = Category.objects.order_by('-likes')[:5]
 
-    # return rendered response to send to client
-    # make use of shortcut function render
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
+    # render response and send it back
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
