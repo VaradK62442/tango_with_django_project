7906b521bea811ccdcc8e278d572882afac1b94d
Varad Kulkarni <varadkulk123@gmail.com>
2024-01-23 17:24:05 +0000

    add show category page

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category
+from rango.models import Page
 
 # each view exists here as a series of individual functions
 # each view takes in at least one argument - a HttpRequest object
@@ -26,3 +27,27 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'Crunch, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    # create context_dict to pass to template rendering engine
+    context_dict = {}
+
+    try:
+        # can we find category name with the given slug?
+        # if we can't, .get() method raises DoesNotExist exception
+        category = Category.objects.get(slug=category_name_slug)
+
+        # retrieve all associated pages
+        pages = Page.objects.filter(category=category)
+
+        # add results list to the template context under name pages
+        context_dict['pages'] = pages
+        # add category object from database to context dict
+        # use this in template to verify that the category exists
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        # template will display "no category" message
+        context_dict['category'], context_dict['pages'] = None, None
+
+    return render(request, 'rango/category.html', context=context_dict)
